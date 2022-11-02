1127008388ef63749f38c8e699df5e0582a56471
Vitalii Travin <vitalii.trvn@gmail.com>
2022-11-02 15:23:17 +0600

    types SelectForm comp

--- src/components/SelectForm.tsx
+++ src/components/SelectForm.tsx
@@ -2,16 +2,16 @@ import React from "react";
 import { TasksType } from "../App";
 import { LIST_TYPES } from "../config";
 
-// type SelectFormProps = {
-//   type: string
-//   setIsFormVisible: React.Dispatch<React.SetStateAction<boolean>>
-//   initTasks: TasksType[]
-//   setTasks: any
-//   setIsButtonVisible: React.Dispatch<React.SetStateAction<boolean>>
-// }
+type SelectFormProps = {
+  type: string
+  setIsFormVisible: React.Dispatch<React.SetStateAction<boolean>>
+  initTasks: TasksType[]
+  setTasks: React.Dispatch<React.SetStateAction<TasksType[]>> 
+  setIsButtonVisible: React.Dispatch<React.SetStateAction<boolean>>
+}
 
-const SelectForm = ({ type, setIsFormVisible, initTasks, setTasks, setIsButtonVisible }) => {
-  const changeType = (type) => {
+const SelectForm: React.FC<SelectFormProps> = ({ type, setIsFormVisible, initTasks, setTasks, setIsButtonVisible }) => {
+  const changeType = (type: string) => {
     switch (type) {
       case LIST_TYPES.READY:
         return initTasks.filter((task) => task.status === LIST_TYPES.BACKLOG);
@@ -26,19 +26,22 @@ const SelectForm = ({ type, setIsFormVisible, initTasks, setTasks, setIsButtonVi
 
   const typeTasks = changeType(type);
 
-  const handleChange = (e) => {
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const newTitle = e.target.value;
     const foundTask = initTasks.find((item) => item.title === newTitle);
-    foundTask.status = type;
-    const updatedTasks = [...initTasks.filter((item) => item.title !== newTitle), foundTask];
-    setTasks(updatedTasks);
-    setIsFormVisible(false);
-    setIsButtonVisible(true);
+    if(foundTask){
+      foundTask.status = type; 
+      const updatedTasks = [...initTasks.filter((item) => item.title !== newTitle), foundTask];
+      setTasks(updatedTasks);
+      setIsFormVisible(false);
+      setIsButtonVisible(true);
+      }
+ 
   };
 
   return (
     <select className='select' onChange={handleChange}>
-      <option defaultValue={false} value=''></option>
+      <option defaultValue={undefined} value=''></option>
       {typeTasks.map((task) => {
         return (
           <option key={task.id} value={task.title}>
